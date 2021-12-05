650241481a4af7edde995f63dd68cd1d7173af5e
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2021-12-05 16:09:43 -0300

    correção de comentário

--- formulario-empresa/src/pages/Comentarios.tsx
+++ formulario-empresa/src/pages/Comentarios.tsx
@@ -27,7 +27,19 @@ export function Comentarios() {
             <div className="py-5 text-center">
                 <img src={comentarioLogo} alt="" width="72" height="57" />
                 <h2>Comentários</h2>
-                <p className="lead">Comentarios sobre suas respostas</p>
+                <div className="card">
+                    <h5 className="card-header">Contexto</h5>
+                    <div className="card-body">
+                        <h5 className="card-title">O que é cada etapa?</h5>
+                        <p className="card-text">O reconhecimento é uma etapa inicial onde o atacante identifica as formas que ele pode atacar a empresa, por isso o ideal é evitar que a empresa e seus funcionarios façam grande exposições sobre a rede da empresa.
+                            O momento que a atacante escolha qual tipo de ataque ele fará na empresa, o ideal é possuir alguma ferramenta capaz de idenficar certo padrões de malwares e assim eliminar a ameaça.
+                            É o momento em que o malware é enviado para o sistema da empresa por meio de spam,arquivos idesejados,pendrives contaminados e entre outros.A principal forma de evitar essa entrega é impedir o acesso a locais que são comprometidos ou que seja colocado pendrives sem validação.
+                            Exploração é onde o atacante utiliza das vulnerabilidade do sistema para executar o codigo, conhecer o sistema e identificar as areas mais vulneraveis é uma das principais maneiras de evitar esta parte.
+                            É o momento de instalação do codigo é importante sempre verificações de rotina pelos arquivos do sistema para impedir a instalação desses malwares.
+                            É o momento do ataque onde o sistema está comprometido é a ultima chance do defensor bloquear o ataque.É interessante utilizar ferramentas de firewall e outros analisadores de rede e utilizar proxys para assegurar a rede e envitar chegar nesta situação.
+                            Quanto o atacante alcança o objetivo neste ponto a unica solução é mitigar os danos identificando quais dados foram comprometidos e o que pode ser fazer, além de caçar o malware e elimina-lo da rede.</p>
+                    </div>
+                </div>
             </div>
             <main className="col-md-12  px-md-4">
                 <>
